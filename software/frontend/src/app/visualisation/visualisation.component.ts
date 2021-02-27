51a9bbd63f9608429303c99b0be18fb66f0e79c1
Henri Heimann <henriheimann@gmail.com>
2021-02-27 16:36:11 +0100

    Add three.js canvas

---
+++ software/frontend/src/app/visualisation/visualisation.component.ts
@@ -0,0 +1,24 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {VisualisationService} from './visualisation.service';
+
+@Component({
+  selector: 'app-visualisation',
+  templateUrl: './visualisation.component.html',
+  styleUrls: ['./visualisation.component.scss']
+})
+export class VisualisationComponent implements OnInit {
+
+  @ViewChild('canvasWrapper', {static: true})
+  canvasWrapper!: ElementRef<HTMLDivElement>;
+
+  @ViewChild('rendererCanvas', {static: true})
+  rendererCanvas!: ElementRef<HTMLCanvasElement>;
+
+  public constructor(private visualisationService: VisualisationService) {
+  }
+
+  public ngOnInit(): void {
+    this.visualisationService.createScene(this.canvasWrapper, this.rendererCanvas);
+    this.visualisationService.animate();
+  }
+}
