ab40959400f48fac28cef208a85a060b45098d91
Henri Heimann <henriheimann@gmail.com>
2021-03-25 14:40:13 +0100

    Change submodule directory

--- software/embedded/sensor-node/Application/application.h
+++ software/embedded/sensor-node/Application/application.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <sht3x/sht3x.h>
-#include <rfm95/rfm95.h>
-#include <eeprom/eeprom.h>
-#include <mlx90614/mlx90614.h>
+#include <sht3x.h>
+#include <rfm95.h>
+#include <eeprom.h>
+#include <mlx90614.h>
 
 extern sht3x_handle_t sht3x_handle;
 extern rfm95_handle_t rfm95_handle;
