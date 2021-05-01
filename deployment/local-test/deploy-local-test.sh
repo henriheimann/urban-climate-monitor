09f64fb3da8e8d046e7c5818f3a3709daefa6864
Henri Heimann <henriheimann@gmail.com>
2021-05-01 13:45:41 +0200

    Fix deploy-local-test script to include user access for metricstest

--- deployment/local-test/deploy-local-test.sh
+++ deployment/local-test/deploy-local-test.sh
@@ -14,3 +14,4 @@ sleep 5
 # import influxdb testdata
 docker cp influxdb-testdata ucm-influxdb:/tmp/testdata
 docker exec -t ucm-influxdb bash -c "influx -username ${INFLUXDB_ADMIN_USER} -password ${INFLUXDB_ADMIN_PASSWORD} -database ${INFLUXDB_TEST_DB} -execute 'DROP DATABASE ${INFLUXDB_TEST_DB}' && influxd restore -portable -db metrics -newdb ${INFLUXDB_TEST_DB} /tmp/testdata"
+docker exec -t ucm-influxdb bash -c "influx -username ${INFLUXDB_ADMIN_USER} -password ${INFLUXDB_ADMIN_PASSWORD} -execute 'GRANT ALL ON \"${INFLUXDB_TEST_DB}\" TO \"${INFLUXDB_USER}\"'"
